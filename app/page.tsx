ff16b86abf24b04b764e4f08f61268aaa18cb758
manoj <sm6361098@gmail.com>
2026-08-24 22:45:51 +0545

    commit db mongo

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,33 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
 export default function Home() {
+  const [dbConnected, setDbConnected ] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const checkDb = async () => {
+      try{
+        const res = await fetch("/api/tasks");
+        const data = await res.json();
+        setDbConnected(data.dbConnected);
+
+      } catch{
+        setDbConnected(false);
+      }
+    };
+    checkDb();
+  },[] );
 
   return (
     <main className="min-h-screen bg-gray-800 flex flex-col items-center p-6">
       <h1 className="text-2xl font-bold mb-6">My to do Application</h1>
-      <p className="text-red-600">mongo atlas is not connected</p>
+
+
+      <p className={dbConnected ?  "text-green-500" : "text-red-600"}>{dbConnected ? "Connected" : "mongo atlas is not connected" }</p>
+
+
       <TodoForm />
       <TodoList />
     </main>
