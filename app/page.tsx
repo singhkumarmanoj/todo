9e5140a94c0a5c9a7c539aa1061b98659b3db31d
manoj <sm6361098@gmail.com>
2026-08-24 19:39:29 +0545

    mongo not connect

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-gray-800 flex flex-col items-center p-6">
       <h1 className="text-2xl font-bold mb-6">My to do Application</h1>
+      <p className="text-red-600">mongo atlas is not connected</p>
       <TodoForm />
       {/* <TodoList /> */}
     </main>
